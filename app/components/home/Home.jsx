8f910a7cb6f092c1aee638dc43e26150f73f22da
Jayson Wong <jaysonwongf@gmail.com>
2024-03-16 20:34:03 +0800

    use client rendering useSession

--- app/components/home/Home.jsx
+++ app/components/home/Home.jsx
@@ -4,27 +4,25 @@ import Link from 'next/link';
 import { Button, Typography } from '@mui/material';
 import { MainContainer } from '@/app/components/layout/styles/globals';
 import { signIn, signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 
-export default function Home({ session }) {
+export default function Home() {
+  const { data: session, status } = useSession();
+
+  console.log(session);
+
   return (
     <MainContainer>
-      {session ? (
-        <>
-          <Button variant="contained" onClick={() => signOut()}>
-            Sign Out
-          </Button>
+      <Button
+        variant="contained"
+        onClick={() => (session ? signOut() : signIn())}
+      >
+        {session ? 'Sign Out' : 'Sign In'}
+      </Button>
 
-          <Typography sx={{ my: 4 }}>Welcome {session.user?.name || 'Guest'}</Typography>
-        </>
-      ) : (
-        <>
-          <Button variant="contained" onClick={() => signIn()}>
-            Sign In
-          </Button>
-
-          <Typography sx={{ my: 4 }}>Welcome Guest</Typography>
-        </>
-      )}
+      <Typography sx={{ my: 4 }}>
+        Welcome {session ? session.user.name : 'Guest'}
+      </Typography>
     </MainContainer>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,13 @@
-import { auth } from '@/auth';
+// import { auth } from '@/auth';
 import Home from './components/home/Home';
 
 export default async function HomePage() {
-  const session = await auth();
+  // const session = await auth();
 
 
   return (
     <>
-      <Home session={session} />
+      <Home  />
     </>
   );
 }
