35a7838320d0c9658256458d6e5cca5ebc12ed7d
Jayson Wong <jaysonwongf@gmail.com>
2024-09-24 14:24:30 +0800

    test remove address, places api

--- app/[locale]/mysettings/[slug]/page.jsx
+++ app/[locale]/mysettings/[slug]/page.jsx
@@ -2,7 +2,11 @@ import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 import MySettingsTabs from '@/app/(features)/account/components/MySettingsTabs';
 
-export default function MySettingsPage({ params }) {
+export default async function MySettingsPage({ params }) {
+  const session = await auth();
+
+  if (!session) redirect('/');
+
   const { slug } = params;
 
   const validSlugs = ['personal', 'preferences', 'security', 'payment'];
